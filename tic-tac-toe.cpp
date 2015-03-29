a46b022182ce1bb8c1c4ce8f3e8043f8ba065f05
Jared Kotoff <krylic@gmail.com>
2015-03-29 15:32:23 -0700

    Board initalization and print functions

--- tic-tac-toe.cpp
+++ tic-tac-toe.cpp
@@ -3,15 +3,52 @@
 using namespace std;
 
 class Board{
-  public:
-    Board(int size);
+    int **cells;
     
-  private:
-    int board_size
+    public:
+        Board(int size);
+        ~Board();
+        Print();
+        
+    
+    private:
+        int board_size;
 };
 
 Board::Board(int size = 3){
-  board_size = size;
+    
+    this->board_size = size;
+    this->cells = new int* [size];
+    for(int i=0; i<size; ++i)
+        cells[i] = new int [size];
+    
+    for(int i =0; i<size; ++i){
+        for(int j=0; j < size; ++j)
+            cells[j][i] = 0;
+    }    
+}
+
+Board::~Board(){
+    for(int i=0; i < board_size; ++i){
+        delete[] cells[i];
+    }
+    delete[] cells;
+    cout << endl << "Array deleted";
+}
+
+Board::Print(){
+    for (int i=0; i < board_size; ++i){
+        for (int j=1; j < board_size; ++j)
+            cout << " " << cells[j][i] << " |";
+        cout << " " << cells[board_size-1][i];
+        cout << endl;
+        if(i != board_size-1){
+            for (int j=1; j < board_size; ++j)
+                cout << "----";
+            cout << "---" << endl;
+        }
+    }
+    return 0;
 }
 
 /*class Player{
@@ -32,5 +69,6 @@ int main(){
   cout << "Pick the size of the grid (default = 3): ";
   cin >> board_size;
   Board board(board_size);
+  board.Print();
   return 0;
 }
